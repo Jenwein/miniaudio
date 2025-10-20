3afa9957fa13afc636d3e4f7b7152c6f5e50983d
Jenwein <112601325+Jenwein@users.noreply.github.com>
2025-10-20 20:47:13 +0800

    完善premake

--- premake5.lua
+++ premake5.lua
@@ -1,1 +1,38 @@
-Project "miniaudio"
+-- premake5.lua for Miniaudio library
+project "miniaudio"
+    kind "StaticLib"
+    language "C"
+    staticruntime "on"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files {
+        "miniaudio.h",
+        "miniaudio_impl.c"
+    }
+
+    	includedirs
+	    {
+		    "include",
+		    "src"
+	    }
+
+    flags { "NoPCH" }
+    filter "system:windows"
+        systemversion "latest"
+        links { "ole32.lib" }
+
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "off"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
+  	
+    filter "configurations:Dist"
+		    runtime "Release"
+		    optimize "on"
+        symbols "off"
